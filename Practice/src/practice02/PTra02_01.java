51ec8e1578e5b01a39be3aa9e15ddf6c694def2e
DESKTOP-NU87N8R\likeit_student <youhei.furudate@gmail.com>
2017-11-06 12:32:06 +0900

    Ptar02_05まで完了

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -11,12 +11,12 @@ public class PTra02_01 {
 
 		// ★ 20を5で割った値を出力してください
 
-
+ System.out.println(20/5);
 		// ★ 126に13を掛けた値を出力してください
 
-
+System.out.println(126*3);
 		// ★ 97を6で割ったときの余りを出力してください
 
-
+System.out.println(97%6);
 	}
 }
