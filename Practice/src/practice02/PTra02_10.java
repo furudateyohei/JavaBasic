f9d8be39bc75a738680b2fe6fea1a10722646699
DESKTOP-NU87N8R\likeit_student <youhei.furudate@gmail.com>
2017-11-07 18:01:49 +0900

    Ptar03_07まで完了

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,14 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+	int total = coffee + snack;	
+    
+    double tax = 1.08;
+    
+    double k = total * tax;
+    
+    int i =(int)k;
+    
+    System.out.println(moneyInHand-i);
 	}
 }
