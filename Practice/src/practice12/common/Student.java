b65ee03b6cca685d195bd9794a88cdd5952e829f
DESKTOP-NU87N8R\likeit_student <youhei.furudate@gmail.com>
2017-11-27 18:32:28 +0900

    PTra12_01まで完了

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -13,20 +13,62 @@ package practice12.common;
  */
 public class Student {
 	/** 受講生ID */
-	public int studentId;
+	private int studentId;
+
+	public int getStudentId() {
+		return this.studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
 
 	/** 受講生氏名 */
-	public String studentName;
+	private String studentName;
+
+	public String getStudentName() {
+		return this.studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
 
 	/** 会社名 */
-	public String companyName;
+	private String companyName;
+
+	public String getCompanyName() {
+		return this.companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
 
 	/** 教室名 */
-	public String className;
+	private String className;
+
+	public String getClassName() {
+		return this.className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
 
 	/** メールアドレス */
-	public String mail;
+	private String mail;
+
+	public String getMail() {
+		return this.mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
 
 	/** パスワード */
-	public String password;
+	private String password;
+
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
